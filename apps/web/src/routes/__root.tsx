3d05edf2c6b911af9f2f2c9c23604497a4c2232a
Dalvae <diego.alvarez.e@ug.uchile.cl>
2026-06-08 16:09:37 -0400

    feat(ui): theme de marca Buenas Migas (cacao/crema, Zilla Slab, light por defecto, graficos calidos)

--- apps/web/src/routes/__root.tsx
+++ apps/web/src/routes/__root.tsx
@@ -53,7 +53,7 @@ function RootComponent() {
 			<HeadContent />
 			<ThemeProvider
 				attribute="class"
-				defaultTheme="dark"
+				defaultTheme="light"
 				disableTransitionOnChange
 				storageKey="vite-ui-theme"
 			>
